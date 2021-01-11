69ae53dacc66494b24458b18b84796aeceda44d8
Ranvijay Kumar <67736054+RanvijayChouhan12@users.noreply.github.com>
2021-01-11 21:24:32 +0530

    Completed Checkout Items
    CheckOut Items Working Great WIth Add, Remove and INCREASE and DECREASE Items in Cart...😍😍😎😎

--- src/Components/Checkout-Items/CheckoutItems.jsx
+++ src/Components/Checkout-Items/CheckoutItems.jsx
@@ -1,18 +1,37 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { clereItemFromCart, addItemstoCart, removeItem } from '../../Redux/Cart/CartAction';
 import './CheckoutItems.scss';
 
-function CheckoutItems({ cartItem: { name, imageUrl, quantity, price } }) {
+function CheckoutItems({ cartItem, clearItem, addItem, removeItem }) {
+	const { name, imageUrl, quantity, price } = cartItem;
 	return (
 		<div className="checkout-item">
 			<div className="image-container">
 				<img src={imageUrl} alt="item" />
 			</div>
 			<span className="name">{name}</span>
-			<span className="quantity">{quantity}</span>
+			<span className="quantity">
+				<div onClick={() => removeItem(cartItem)} className="arrow">
+					&#10094;
+				</div>
+				<span className="value">{quantity}</span>
+				<div onClick={() => addItem(cartItem)} className="arrow">
+					&#10095;
+				</div>
+			</span>
 			<span className="price">{price}</span>
-			<div className="remove-button">&#9940;</div>
+			<div onClick={() => clearItem(cartItem)} className="remove-button">
+				&#9940;
+			</div>
 		</div>
 	);
 }
 
-export default CheckoutItems;
+const mapDispatchToProps = (dispatch) => ({
+	clearItem: (item) => dispatch(clereItemFromCart(item)),
+	addItem: (item) => dispatch(addItemstoCart(item)),
+	removeItem: (item) => dispatch(removeItem(item))
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItems);
